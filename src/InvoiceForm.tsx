58d224e68d692532505a5037902a1f8263e87381
Newaz Nezif <newaznezif@gmail.com>
2026-05-05 22:27:58 +0300

    fix: make login page full-screen to fix layout stretching

--- src/InvoiceForm.tsx
+++ src/InvoiceForm.tsx
@@ -22,7 +22,12 @@ const Login: React.FC<{ onLogin: () => void }> = ({ onLogin }) => {
 
   return (
     <div style={{
-      minHeight: '100vh',
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      zIndex: 9999,
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center',
