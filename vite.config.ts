17004f0766d8a1669af8436d67969f6a7b5742db
Newaz Nezif <newaznezif@gmail.com>
2026-05-07 14:35:12 +0300

    fix: add permissive CSP headers to vite dev server

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'Content-Security-Policy': "default-src 'self' * data: blob: 'unsafe-inline' 'unsafe-eval'; script-src 'self' * data: blob: 'unsafe-inline' 'unsafe-eval' 'wasm-unsafe-eval' 'inline-speculation-rules'; style-src 'self' * 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' * data: blob: https://fonts.gstatic.com;"
+    }
+  }
 })
